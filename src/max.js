cac2b3d11dd131e38e9e5584452cef5cd5b507db
Emran <4305563+xmanemran@users.noreply.github.com>
2021-08-18 01:43:29 +0600

    add max

---
+++ src/max.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Max extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
