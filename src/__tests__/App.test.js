7bfdff54914ed47bc41a4a592735f6e5babe17de
Emran <4305563+xmanemran@users.noreply.github.com>
2021-08-18 01:38:51 +0600

    Add sonar

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 // import the React Test Renderer
-import { create } from "react-test-renderer";
+import { create } from 'react-test-renderer';
 // test card state renders
 
 it('renders without crashing', () => {
